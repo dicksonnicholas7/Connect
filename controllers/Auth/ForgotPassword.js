e8c70c9a7f453cbb2e4aec04d4861ac589f27445
Nicholas Dickson <58767603+dicksonnicholas7@users.noreply.github.com>
2020-07-14 14:16:24 +0000

    Create ForgotPassword.js

---
+++ controllers/Auth/ForgotPassword.js
@@ -0,0 +1,148 @@
+const { Op } = require('sequelize');
+const User = require('../../models').User;
+const UserAccount = require('../../models').UserAccount;
+const crypto = require('crypto');
+let secret = "group3";
+const {SendMailResetPassword} = require('./ForgotPasswordMaill');
+
+//render forgot password page
+module.exports.GetForgotPassword = async (req, res, next) => {
+
+    let usertype = 0
+    let show = false;
+
+    if(!res.locals.user){
+        show = false;
+    }else{
+        show = true;
+         usertype = res.locals.user.UserAccount.RoleId;
+    }
+
+
+    res.locals.display = false;
+    res.render(
+        'auth/forgot-password',
+        {
+            usertype,
+            show,
+            page: 'forgot-password'
+        }
+    );
+};  
+
+//render reset password page
+module.exports.GetResetPassword = async (req, res, next) => {
+    let usertype = 0
+    let show = false;
+
+    if(!res.locals.user){
+        show = false;
+    }else{
+        show = true;
+         usertype = res.locals.user.UserAccount.RoleId;
+    }
+
+
+    res.locals.display = false;
+    let token = req.params.token;
+    let email = req.params.email;
+    res.locals.email = email;
+    res.locals.correct = false;
+    (token === hashPassword(email)) ? res.locals.correct = true : res.locals.correct = false;
+    res.render(
+        'auth/reset-password',
+        {
+            usertype,
+            show,
+            correct:res.locals.correct,
+            page: 'reset-password'
+        }
+    );
+};
+
+// perform password reset by generating a token plus a link amd sending to user email if email exists
+module.exports.forgotPasswordEmail = async (req,res,next)=>{
+    let usertype = 0
+    let show = false;
+
+    if(!res.locals.user){
+        show = false;
+    }else{
+        show = true;
+         usertype = res.locals.user.UserAccount.RoleId;
+    }
+
+
+    res.locals.display = true;
+    let userAccount = {
+        email: req.body.email
+    };
+
+    let ret_user = await User.findOne({ where:{email:userAccount.email} });
+    if(ret_user!==null) {
+        let token = hashPassword(userAccount.email);
+        console.log("Token:  " + token);
+        let hostname = req.headers.host;
+        //send reset link to email
+        res.locals.emailSent = !!SendMailResetPassword(userAccount.email, token, hostname);
+        res.render(
+            'auth/forgot-password',
+            {
+                emailSent: res.locals.emailSent,
+                page: 'forgot-password'
+            }
+        )
+    }else{
+        res.locals.emailNotFound = true;
+        res.render(
+            'auth/forgot-password',
+            {
+                usertype,
+                show,
+                emailNotFound: res.locals.emailNotFound,
+                page: 'forgot-password'
+            }
+        )
+    }
+};
+
+//perform password
+module.exports.DoResetPassword = async (req,res,next)=>{
+    let usertype = 0
+    let show = false;
+
+    if(!res.locals.user){
+        show = false;
+    }else{
+        show = true;
+         usertype = res.locals.user.UserAccount.RoleId;
+    }
+
+    res.locals.display = true;
+    res.locals.correct = true;
+    let email = req.body.email;
+    let newPassword = {
+        password: hashPassword(req.body.password)
+    };
+    let user_info = await User.findOne({ where:{email:email} });
+    let upd_userAccount = await UserAccount.update(newPassword,{ where: {UserId: user_info.id} });
+    res.locals.changed = false;
+    (upd_userAccount!==null) ? res.locals.changed = true : res.locals.changed = false;
+    console.log("changed");
+    res.render(
+        'auth/reset-password',
+        {
+            usertype,
+            show,
+            changed:res.locals.changed,
+            page: 'reset-password'
+        }
+    );
+};
+
+//hash password
+hashPassword = (password) =>{
+    return crypto.createHmac('sha256', secret)
+        .update(password)
+        .digest('hex');
+};
