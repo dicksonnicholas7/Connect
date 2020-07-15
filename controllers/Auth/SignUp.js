cc83c15b2486fd6725f9d91611b5bedcbab3a92f
Nicholas Dickson <58767603+dicksonnicholas7@users.noreply.github.com>
2020-07-15 13:29:24 +0000

    Update SignUp.js

--- controllers/Auth/SignUp.js
+++ controllers/Auth/SignUp.js
@@ -20,9 +20,9 @@ module.exports.GetBusinessSignUp = (req, res, next ) => {
         show = false;
     }else{
         show = true;
-         usertype = res.locals.user.UserAccount.RoleId;
+        usertype = res.locals.user.UserAccount.RoleId;
     }
-    
+
 
     console.log(usertype);
 
@@ -30,17 +30,17 @@ module.exports.GetBusinessSignUp = (req, res, next ) => {
 
     if(req.session.loggedIn===true){
         res.redirect('/');
-    }else{ 
+    }else{
         res.render(
-                'auth/business-signup',
-                {
-                    usertype,
-                    show,
-                    page:'business-signup',
-                    signUpErrorMessage:'',
-                    signUpSuccessMessage:''
-                }
-            )
+            'auth/business-signup',
+            {
+                usertype,
+                show,
+                page:'business-signup',
+                signUpErrorMessage:'',
+                signUpSuccessMessage:''
+            }
+        )
     }
 };
 
@@ -123,7 +123,6 @@ module.exports.DoSignUp = async (req, res, next) => {
         ]
     };
 
-    if(user_type === 2){
             //check if email is already used
     let user = await User.findOne({ where:{email:req.body.email} });
     if(user!==null && user.email===req.body.email){
@@ -164,46 +163,84 @@ module.exports.DoSignUp = async (req, res, next) => {
             getSignUpPage(req, res, next, error, success );
         }
     }
-    }else if (user_type === 1){
+    }else{
+        let error = 'Please enter a valid email address';
+        let success = '';
+        getSignUpPage(req, res, next, error, success );
+
+    }
+};
+
+
+
+
+module.exports.DoBusinessSignUp = async (req, res, next) => {
+    let usertype = 0
+    let show = false;
+
+    if(!res.locals.user){
+        show = false;
+    }else{
+        show = true;
+         usertype = res.locals.user.UserAccount.RoleId;
+    }
+    
+    //generate a token for email verification
+    if(validator.validate(req.body.email)){
+        
+    const token = jwt.sign(
+        { userId: req.body.email },
+        'connect',
+        { expiresIn: '24h' });
+
+        let user_type = 0;
+
+        if(req.originalUrl=='/signup'){
+            user_type = 2;
+        }else if(req.originalUrl=='/business-signup'){
+            user_type = 1;
+        }
+
+        console.log(user_type);
+        console.log(req.originalUrl)
+
+    let userInfo = {
+        email: req.body.email || '',
+        mobile: req.body.mobile || '',
+        UserAccount: [
+            {
+                username: '',
+                password: hashPassword(req.body.password),
+                RoleId: req.body.role,
+                UserTypeId: user_type,
+                verified: false,
+                token: token,
+            }
+        ]
+    };
             //check if email is already used
     let user = await BusinessUser.findOne({ where:{email:req.body.email} });
     if(user!==null && user.email===req.body.email){
         console.log("Business User already exists. Log in");
-        res.render(
-            'auth/business-sign-up',
-            {
-                page:'business-signup',
-                signUpErrorMessage: 'User by that email already exists'
-            }
-        );
+        let error = 'Business User already exists. Log in';
+        let success = '';
+        getBusinessSignUpPage(req, res, next, error, success);
     }else{
         let bus_Account = await BusinessUser.create(userInfo, { include: [UserAccount] } );
         if(bus_Account.id!==null){
-            //set up portfolio for user
-            let bus_Portfolio = await Portfolio.create({BusinessPortId:bus_Account.id});
-            if(bus_Portfolio.id!==null) {
-                console.log("Business Account Created successfully");
-                let hostname = req.headers.host;
-                try{
-                    req.session.uemail = userInfo.email;
-                    req.session.utoken = token;
-                }catch(e){
-                    console.log(e);
-                }
-                //send verification email
-                SendMailVerify(userInfo.email, token, hostname);
-                res.render("auth/success-register",{page:'business-signup',signUpSuccessMessage:'An email has been sent to your account to verify.'});
-            }else{
-                console.log("Profile could not be created");
-                req.session.signUpErrorMessage = "Error creating account";
-                res.render(
-                    'auth/business-sign-up',
-                    {
-                        page:'business-signup',
-                        signUpErrorMessage: 'Error creating Business Account'
-                    }
-                );
+           
+            console.log("Business Account Created successfully");
+            let hostname = req.headers.host;
+            try{
+                req.session.uemail = userInfo.email;
+                req.session.utoken = token;
+            }catch(e){
+                console.log(e);
             }
+            //send verification email
+            SendMailVerify(userInfo.email, token, hostname);
+            res.render("auth/success-register",{page:'business-signup',signUpSuccessMessage:'An email has been sent to your account to verify.'});
+
         }else{
             console.log("Error creating account ");
             res.render(
@@ -215,14 +252,14 @@ module.exports.DoSignUp = async (req, res, next) => {
             );
         }
     }
-    }
     }else{
-        let error = 'Please enter a valid email address';
+        let error = 'Please enter a valid Business email address';
         let success = '';
         getSignUpPage(req, res, next, error, success );
 
     }
 };
+
 
 
 
@@ -255,6 +292,40 @@ getSignUpPage = (req, res, next, signUpErrorMessage, signUpSuccessMessage ) => {
                     signUpSuccessMessage:signUpSuccessMessage
                 }
             )
+    }
+}
+
+
+
+getBusinessSignUpPage = (req, res, next, signUpErrorMessage, signUpSuccessMessage ) => {
+    let usertype = 0
+    let show = false;
+
+    if(!res.locals.user){
+        show = false;
+    }else{
+        show = true;
+        usertype = res.locals.user.UserAccount.RoleId;
+    }
+
+
+    console.log(usertype);
+
+    //render sign up page
+
+    if(req.session.loggedIn===true){
+        res.redirect('/');
+    }else{
+        res.render(
+            'auth/business-signup',
+            {
+                usertype,
+                show,
+                page:'business-signup',
+                signUpErrorMessage:'',
+                signUpSuccessMessage:''
+            }
+        )
     }
 }
 
