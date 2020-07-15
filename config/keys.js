fe444ab5954ca2f110bdbf1b13efc30470c2f0ae
Nicholas Dickson <58767603+dicksonnicholas7@users.noreply.github.com>
2020-07-15 16:18:16 +0000

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-    client_id: "496893632690-o7p2enqal4fr7g3ekka1u23lnr8df1t4.apps.googleusercontent.com",
-    client_secret: "S3nb85ZLDXhEZyyqW4EA-_54",
-    refresh_token: "1//04NFUbHs-0avoCgYIARAAGAQSNwF-L9IrwLPoaTqq2HpuJCw4bWwmxWXWBbEVDwUtSGr_gOzV10H2YX29G-ZozwQ2fuCqxoRFPsQ",
+    client_id: "766797919845-7r8db9pglntbum2orlaees6p888pni8j.apps.googleusercontent.com",
+    client_secret: "QbOtUc4qBlJzX8U-oIV-ei3D",
+    refresh_token: "1//04c4JxOQYJRUUCgYIARAAGAQSNwF-L9Irx7clGuDGN5rVWTfzRzA8tQyq0mUVSJgncqKD0XTVyGgxfqdKH8wBoK9yqgSdND4etyk",
     stripeKeyPublishable: 'pk_test_cGawfDbRwNlwGc7L9OHxVLwB00AvTIgTkg',
     stripeKeySecret: 'sk_test_RDIoGjkXBg7304rnk6NaypWd00MxIWl5U4',
 };
