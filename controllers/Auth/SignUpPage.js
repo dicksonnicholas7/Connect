26f2307f75c6f656f29e4132155880edfb30e4e1
Nicholas Dickson <58767603+dicksonnicholas7@users.noreply.github.com>
2020-07-15 11:57:35 +0000

    Create SignUpPage.js

---
+++ controllers/Auth/SignUpPage.js
@@ -0,0 +1,25 @@
+module.exports.GetSignUpPage = (req, res, next ) => {
+    let usertype = 0
+    let show = false;
+
+    if(!res.locals.user){
+        show = false;
+    }else{
+        show = true;
+         usertype = res.locals.user.UserAccount.RoleId;
+    }
+    
+    //render sign up page
+
+    if(req.session.loggedIn===true){
+        res.redirect('/');
+    }else{ 
+        res.render(
+                'auth/signup-page',
+                {
+                    usertype,
+                    show,
+                    page:'signup-page',
+            )
+    }
+};
