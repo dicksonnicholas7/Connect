5b74b7235e7117bfdae74e9c39972d5960f78b7c
clement <ckoomson75@gmail.com>
2020-07-20 22:29:40 +0000

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use( (req, res, next) => {
 });
 
 app.use('/', indexRouter);
-app.use('/user', checkLoggedIn, usersRouter);
+app.use('/user', usersRouter);
 app.use('/admin', checkLoggedIn, adminRouter);
 
 // catch 404 and forward to error handler
