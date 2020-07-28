3a9b74bff5b68e1afe42e44636abe0790d285a9e
Nicholas Dickson <58767603+dicksonnicholas7@users.noreply.github.com>
2020-07-28 11:14:22 +0000

    changes

--- controllers/Auth/VerificationEmail.js
+++ controllers/Auth/VerificationEmail.js
@@ -2,6 +2,8 @@ const nodeMailer = require('nodemailer');
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const {client_id, client_secret, refresh_token} = require('../../config/keys');
+var fs = require("fs");
+var ejs = require("ejs");
 
 //set up google OAuth2 client
 const oauth2Client = new OAuth2(
@@ -17,7 +19,7 @@ oauth2Client.setCredentials({
 const accessToken = oauth2Client.getAccessToken();
 
 //send email for verification
-module.exports.SendMailVerify = (emailReceiver, token, hostname)=>{
+module.exports.SendMailVerify = async (emailReceiver, token, hostname)=>{
     let transporter = nodeMailer.createTransport({
         service: "gmail",
         auth: {
@@ -29,21 +31,47 @@ module.exports.SendMailVerify = (emailReceiver, token, hostname)=>{
             accessToken: accessToken
         }
     });
-    const mailOptions = {
-        to: emailReceiver,
-        from: 'Connect',
-        subject: 'Connect - Verify your email',
-        html: '<div style="background-color:white;color:black;">'+
-        '<p style="font-weight: bold;">Welcome to Connect.<p></p>Click on the following link to verify your email address.<p>'+
-        '<a href="http://'+hostname+'/verification/'+emailReceiver+'/'+token+'">Click here to verify</a></div>',
-    };
-    transporter.sendMail(mailOptions)
-        .then(() => {
-            console.log("Email sent successfully");
-            return 1;
-        }).catch((err) => {
-        console.log("email not sent "+err.message);
-        return (err.message);
-    });
+    // const mailOptions = {
+    //     to: emailReceiver,
+    //     from: 'Connect',
+    //     subject: 'Connect - Verify your email',
+    //     html: '<div style="background-color:white;color:black;">'+
+    //     '<p style="font-weight: bold;">Welcome to Connect.<p></p>Click on the following link to verify your email address.<p>'+
+    //     '<a href="http://'+hostname+'/verification/'+emailReceiver+'/'+token+'">Click here to verify</a></div>',
+    // };
+    // transporter.sendMail(mailOptions)
+    //     .then(() => {
+    //         console.log("Email sent successfully");
+    //         return 1;
+    //     }).catch((err) => {
+    //     console.log("email not sent "+err.message);
+    //     return (err.message);
+    // });
+
+
+
+    ejs.renderFile('C:\\Users\\AMALITECH-PC\\Documents\\new\\Connect\\controllers\\Auth/email-verify.ejs', { hostname: hostname , emailReceiver:emailReceiver, token:token}, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var mainOptions = {
+                to: emailReceiver,
+                from: 'Connect',
+                subject: 'Connect - Verify your email',
+                html: data
+            };
+            //console.log("html data ======================>", mainOptions.html);
+    
+            transporter.sendMail(mainOptions, function (err, info) {
+              if (err) {
+                console.log("Email sent successfully");
+                return 1;
+              } else {
+                console.log("email not sent "+err.message);
+                return (err.message);
+              }
+          });
+          }
+      });
 
 };
