bcbaebd0d888432626bfcc690bf4e9baf47c275b
Nicholas Dickson <58767603+dicksonnicholas7@users.noreply.github.com>
2020-07-29 11:56:25 +0000

    added controllers for find all freelancers

--- controllers/Job/JobFreelancer.js
+++ controllers/Job/JobFreelancer.js
@@ -219,7 +219,9 @@ console.log(job_details)
    
 module.exports.GetAllJobsFreelancer = async (req, res, next) =>{
 
-    
+    let job_skills
+
+
     let sql = "SELECT jobs.*,  users.firstname, users.city, users.country "+ 
     "FROM jobs "+
     "LEFT JOIN useraccounts ON useraccounts.id = jobs.ClientId "+
@@ -227,7 +229,16 @@ module.exports.GetAllJobsFreelancer = async (req, res, next) =>{
 
     const [jobs, metadata] = await db.sequelize.query(sql);
 
-    let job_skills = await JobSkills.findAll({where:{JobId:jobs[0].id}});
+
+    console.log(jobs.length)
+
+
+    if(jobs.length === 0){
+     console.log('no jobs');
+    }else{
+        job_skills = await JobSkills.findAll({where:{JobId:jobs[0].id}});   
+    }
+   
 
 
     if(job_skills !== null ){
