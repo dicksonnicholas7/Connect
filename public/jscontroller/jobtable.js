b3d4a33ee903810289ebc8c7cef02168d6a85212
ebenezer-aidoo <58732236+ebenezer-aidoo@users.noreply.github.com>
2020-07-29 17:09:58 +0000

    worked on the jobs page

--- public/jscontroller/jobtable.js
+++ public/jscontroller/jobtable.js
@@ -1,167 +1,50 @@
 $(document).ready(function() {
-    let appId = "";
-    let jobUserId = "";
-    $("#client_jobs_tb").DataTable({
-        "pagingType": "numbers",
-        "pageLength": 10,
-        "ordering": true,
-        "info":     false,
-        "searching": true,
-        "lengthChange": false,
-        "language": {
-            "emptyTable": "No jobs yet."
-        }
-    });
 
-    $("#client_jobs_tb tbody").on("click","tr",function(){
-        let table = $("#client_jobs_tb").DataTable();
-        let data = table.row(this).data();
-        let jobId = data[1];
-        window.location = "/user/view-job/"+jobId;
-
-    });
-
-    //dashboard jobs
-    $("#client_dasboard_jobs_tb").DataTable({
-        "paging": false,
-        "pagingType": "numbers",
-        "pageLength": 5,
+     let table = $("#jobs_all_tb").DataTable({
+        "pagingType": "simple",
+        "pageLength": 7,
+        "dom": "lrtip",
         "ordering": false,
         "info":     false,
-        "searching": false,
-        "lengthChange": false,
-        "language": {
-            "emptyTable": "No applicants yet."
-        }
-    });
-
-    $("#client_dasboard_jobs_tb tbody").on("click","tr",function(){
-        let table = $("#client_dasboard_jobs_tb").DataTable();
-        let data = table.row(this).data();
-        let jobId = data[4];
-        window.location = "/user/view-job/"+jobId;
-
-    });
-
-    let table = $("#jobs_all_tb").DataTable({
-        "pagingType": "numbers",
-        "pageLength": 7,
-        "ordering": true,
-        "info":     false,
         "searching": true,
         "lengthChange": false,
     });
 
-    $('#searchFilter').on( 'keyup', function () {
+    $("#jobs_all_tb_next").html("");
+    $("#jobs_all_tb_previous").html("");
+    $("#jobs_all_tb_next").addClass("fa fa-angle-right");
+    $("#jobs_all_tb_previous").addClass("fa fa-angle-left");
+
+    function colFilter(searchValue, col){
+        table.columns(parseInt(col)).search(searchValue).draw();
+    };
+    
+    $('#job_search').on( 'keyup', function () {
         table.search( this.value ).draw();
-     } );
+     });
 
-    $("#freelancer_jobs_tb").DataTable({
-        "pagingType": "numbers",
-        "pageLength": 10,
-        "ordering": true,
-        "info":     false,
-        "searching": true,
-        "lengthChange": false,
-        "language": {
-            "emptyTable": "No jobs yet."
-        }
-    });
-
-    $("#freelancer_jobs_tb tbody").on("click","tr",function(){
-        let table = $("#freelancer_jobs_tb").DataTable();
+     $("#jobs_all_tb tbody").on("click", "tr", function(){
         let data = table.row(this).data();
-        let jobId = data[1];
-        let jobAppStatus = data[7];
-    });
+        let jobId = data[0];
 
-    $("#app_tb").DataTable({
-        "pagingType": "numbers",
-        "pageLength": 10,
-        "searching":false,
-        "ordering": true,
-        "info":     false,
-        "lengthChange": false,
-    });
-
-    $("#app_tb tbody").on("click","tr",function(){
-        let table = $("#app_tb").DataTable();
-        let data = table.row(this).data();
-        appId = data[1];
-        jobUserId = data[6];
         $.ajax({
-            url:'/user/view-freel/'+jobUserId,
+            url:'/user/job/'+jobId,
             type:'get',
-            data:{jobUserId: jobUserId},
+            data:{"jobId": jobId},
             success:function(response){
-                $("#firstname").text(response.User.firstname);
-                $("#lastname").text(response.User.lastname);
-                $("#country").text(response.User.country);
-                $("#btnsendfree").attr("href", "/freelancer/"+response.User.id);
-                $("#pic").attr("src", "/img/avatar.jpg");
-                if(response.User.picture!==""){
-                    $("#pic").attr("src", "/images/"+response.User.picture);
-                }else{
-                    
-                }
-                
+                $("#jobId").text(response.id);
+                $("#job_title").text(response.title);
+                $("#postedBy").text(response.firstname);
+                $("#cityCountry").text(response.city +", "+ response.country);
+                $("#description").text(response.details);
+                $("#price").text(response.price);
+                // $("#skills").text(response.job_title);
             }
         });
 
-        //$("freelancer-info").show();
-    });
 
-    $("#btnaward").click((e)=>{
-        window.location = "/user/award-job/"+appId;
-    });
 
-    $("#jobview_tb").DataTable({
-        "pagingType": "numbers",
-        "pageLength": 1,
-        "ordering": false,
-        "info":     false,
-        "searching": false,
-        "lengthChange": false,
-        "language": {
-            "emptyTable": "No jobs have been posted yet."
-        }
-    });
+     });
 
-    $("#message_users_tb").DataTable({
-        "pagingType": "numbers",
-        "pageLength": 10,
-        "ordering": true,
-        "info":     false,
-        "searching": true,
-        "lengthChange": true,
-        "language": {
-            "emptyTable": "No users have joined yet."
-        }
-    });
-
-    $("#message_other_tb").DataTable({
-        "paging":false,
-        "pageLength": 5,
-        "ordering": false,
-        "info":     false,
-        "searching": false,
-        "lengthChange": false,
-        "language": {
-            "emptyTable": "No message received yet."
-        }
-    });
-
-    $("#priceApply").click((e)=>{
-        let table = $('#jobview_tb').DataTable();
-        let minPrice = $("#minPrice").val();
-        let maxPrice = $("#maxPrice").val();
-        table
-            .column(3)
-            .data()
-            .filter( function ( value, index ) {
-                return (value > minPrice && value <= maxPrice);
-            });
-        table.draw();
-    });
 
 });
